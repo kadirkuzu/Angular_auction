140457313e7e94c59c571e23141d59cca54da397
kadirkuzu <kadirkuzu.5656@gmail.com>
2022-06-09 00:05:01 +0300

    "First commit"

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomePageComponent} from "./Pages/home-page/home-page.component";
+import {AddAdComponent} from "./Pages/add-ad/add-ad.component";
+import {AuctionsPageComponent} from "./Pages/auctions-page/auctions-page.component";
+import {LoginComponent} from "./Pages/login/login.component";
+import {LogOutComponent} from "./Pages/log-out/log-out.component";
+import {SignUpComponent} from "./Pages/sign-up/sign-up.component";
+import {TheAuctionPageComponent} from "./Pages/the-auction-page/the-auction-page.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'home',component:HomePageComponent},
+  {path:'',redirectTo:'home',pathMatch:'full'},
+  {path:'add-ad',component:AddAdComponent},
+  {path:'auctions',component:AuctionsPageComponent},
+  {path:'login',component:LoginComponent},
+  {path:'logout',component:LogOutComponent},
+  {path:'sign-up',component:SignUpComponent},
+  {path:'auctions/:auction',component:TheAuctionPageComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './Pages/home-page/home-page.component';
+import { TheAuctionPageComponent } from './Pages/the-auction-page/the-auction-page.component';
+import { AuctionsPageComponent } from './Pages/auctions-page/auctions-page.component';
+import { LoginComponent } from './Pages/login/login.component';
+import { LogOutComponent } from './Pages/log-out/log-out.component';
+import { SignUpComponent } from './Pages/sign-up/sign-up.component';
+import { CategoriesComponent } from './categories/categories.component';
+import { AddAdComponent } from './Pages/add-ad/add-ad.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    TheAuctionPageComponent,
+    AuctionsPageComponent,
+    LoginComponent,
+    LogOutComponent,
+    SignUpComponent,
+    CategoriesComponent,
+    AddAdComponent
   ],
   imports: [
     BrowserModule,
